7e7c39ca3c2c628e9e70459a6b395d65cf8321c4
Dr. Gavin T.D. Greig <ggreig@ggreig.com>
2015-07-08 01:21:24 +0100

    Improvements to documentation.

--- OXO/Program.cs
+++ OXO/Program.cs
@@ -19,6 +19,10 @@ namespace GavinGreig.OXO
     /// <summary>
     /// This class contains the static method Main, which is the entry point of the program.
     /// </summary>
+    /// <remarks>
+    /// The Program class is public by default, but can be made internal without affecting program functionality.
+    /// A class at namespace scope cannot be restricted further.
+    /// </remarks>
     internal class Program
     {
         /// <summary>
@@ -27,7 +31,7 @@ namespace GavinGreig.OXO
         /// </summary>
         /// <param name="args">The arguments.</param>
         /// <remarks>
-        /// The Main method is private by default.
+        /// The Main method is private by default, but we've made that explicit for clarity.
         /// </remarks>
         [System.Diagnostics.CodeAnalysis.SuppressMessage(
             "Microsoft.Usage", 
--- README.md
+++ README.md
@@ -7,6 +7,17 @@ A Noughts and Crosses game where the computer plays against itself.
 
 Written by Dr. Gavin T.D. Greig, July 2015.
 
+Dependencies
+------------
+The projects in this solution depend on the NuGet package manager, 
+and NuGet packages that apply StyleCop style-checking rules to the
+source code. An external installation of StyleCop should not be
+required.
+
+The Unit Test project also depends on packages that integrate NUnit
+functionality into Visual Studio. An external installation of NUnit
+should not be required.
+
 Impediments
 -----------
 * First time using GitHub
